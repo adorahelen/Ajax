785776dc6bf9e1b0de3c9f5d49c0cb9a39c57569
adorahelen <adorahelenmin@gmail.com>
2024-08-28 10:52:14 +0900

    Initial commit

---
+++ src/test/java/com/example/Ajax/AjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
